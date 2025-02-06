7e0934a9cef43800ae93a011dc2975346fae2954
Kongduino <dbvic@me.com>
2025-02-07 07:59:14 +0800

    Simplified test.py (tests in a loop)

--- test.py
+++ test.py
@@ -9,50 +9,33 @@ key = bytes(16)
 keyB = cast(key, POINTER(c_char))
 aes.fillRandom(keyB, 16)
 
-#plain = b"Oh hai there !  "
-plain = b"Hey whassup? This is an unpadded string!"
-finalLength = len(plain)
-extraLength = aes.padLength(len(plain))
-if extraLength > 0:
-  plain += bytes(finalLength)
-  finalLength += extraLength
-plainB = cast(plain, POINTER(c_char))
-if finalLength > 0:
-  aes.PKCS7(plainB, finalLength, extraLength)
-
 print("\n\nECB\n=====")
-aes.AES_init_ctx(ctxB, keyB)
-print(" • Key:")
-hexDump(key)
-print(" • Plaintext:")
-hexDump(plainB[0:finalLength])
-aes.AES_ECB_encrypt_buffer(ctxB, plainB, finalLength)
-print(" • Encrypted:")
-hexDump(plainB[0:finalLength])
-aes.AES_ECB_decrypt_buffer(ctxB, plainB, finalLength)
-print(" • Decrypted:")
-hexDump(plainB[0:finalLength])
-
-plain = b"Hey whassup? This is an unpadded string!"
-finalLength = len(plain)
-extraLength = aes.padLength(len(plain))
-if extraLength > 0:
-  plain += bytes(finalLength)
-  finalLength += extraLength
-plainB = cast(plain, POINTER(c_char))
-if finalLength > 0:
-  aes.PKCS7(plainB, finalLength, extraLength)
-
-aes.AES_init_ctx(ctxB, keyB)
-aes.AES_ECB_encrypt_buffer(ctxB, plainB, finalLength)
-print(" • Encrypted:")
-hexDump(plainB[0:finalLength])
-aes.AES_ECB_decrypt_buffer(ctxB, plainB, finalLength)
-print(" • Decrypted:")
-hexDump(plainB[0:finalLength])
+for i in range(0, 3):
+  plain = b"Hey whassup? This is an unpadded string!\x00"
+  finalLength = len(plain)
+  extraLength = aes.padLength(len(plain))
+  if extraLength > 0:
+    plain += bytes(finalLength)
+    finalLength += extraLength
+  plainB = cast(plain, POINTER(c_char))
+  if finalLength > 0:
+    aes.PKCS7(plainB, finalLength, extraLength)
+  
+  aes.AES_init_ctx(ctxB, keyB)
+  print(" • Key:")
+  hexDump(key)
+  print(" • Plaintext:")
+  hexDump(plainB[0:finalLength])
+  aes.AES_ECB_encrypt_buffer(ctxB, plainB, finalLength)
+  print(" • Encrypted:")
+  hexDump(plainB[0:finalLength])
+  aes.AES_ECB_decrypt_buffer(ctxB, plainB, finalLength)
+  print(" • Decrypted:")
+  hexDump(plainB[0:finalLength])
+  print("")
 
 print("\n\nCBC\n=====")
-plain = b"Hey whassup? This is an unpadded string!"
+plain = b"Hey whassup? This is an unpadded string!\x00"
 finalLength = len(plain)
 extraLength = aes.padLength(len(plain))
 if extraLength > 0:
@@ -76,6 +59,35 @@ for i in range(0, 3):
   hexDump(plainB[0:finalLength])
   aes.AES_init_ctx_iv(ctxB, keyB, ivB)
   aes.AES_CBC_decrypt_buffer(ctxB, plainB, finalLength)
+  print(" • Decrypted:")
+  hexDump(plainB[0:finalLength])
+  print("")
+
+print("\n\nCTR\n=====")
+plain = b"Hey whassup? This is an unpadded string!\x00"
+finalLength = len(plain)
+extraLength = aes.padLength(len(plain))
+if extraLength > 0:
+  plain += bytes(finalLength)
+  finalLength += extraLength
+plainB = cast(plain, POINTER(c_char))
+if finalLength > 0:
+  aes.PKCS7(plainB, finalLength, extraLength)
+print(" • Plaintext:")
+hexDump(plainB[0:finalLength])
+
+for i in range(0, 3):
+  IV = bytes(16)
+  ivB = cast(IV, POINTER(c_ubyte))
+  aes.fillRandom(ivB, 16)
+  print(" • IV:")
+  hexDump(ivB[0:16])
+  aes.AES_init_ctx_iv(ctxB, keyB, ivB)
+  aes.AES_CTR_xcrypt_buffer(ctxB, plainB, finalLength)
+  print(" • Encrypted:")
+  hexDump(plainB[0:finalLength])
+  aes.AES_init_ctx_iv(ctxB, keyB, ivB)
+  aes.AES_CTR_xcrypt_buffer(ctxB, plainB, finalLength)
   print(" • Decrypted:")
   hexDump(plainB[0:finalLength])
   print("")
